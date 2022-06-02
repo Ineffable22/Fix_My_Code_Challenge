edc7d4e9877ab935de9d24fdd5c8a44e73a3ab9b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-01 23:05:17 -0500

    Add: task square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Class Square"""
 
-class square():
 
+class Square():
+    """Object square"""
     width = 0
     height = 0
 
@@ -16,15 +18,17 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Informal string """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
